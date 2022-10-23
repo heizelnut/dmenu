938ce1d74326362de769f4f1f594217dcd6ca72a
heizelnut <upbeatshaw@protonmail.com>
2022-10-23 11:41:35 +0200

    change color scheme

--- config.h
+++ config.h
@@ -1,24 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Inconsolata Nerd Font Mono:size=12",
+	"Iosevka Nerd Font Mono:size=12",
 	"FontAwesome:size=12",
 	"Apple Color Emoji:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*               fg         bg       */
-	[SchemeNorm] = { "#a4a4a4", "#000000" },
-	[SchemeSel] = { "#fafafa", "#181818" },
+	[SchemeNorm] = { "#3c3836", "#f9f5d7" },
+	[SchemeSel] = { "#f9f5d7", "#3c3836" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
+static unsigned int columns    = 0;
+static unsigned int lineheight = 24;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
